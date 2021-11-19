0ee8081b5a731ee883395cbd23a466ac16a17a4d
ethan <ethan.chapman0@outlook.com>
2021-11-19 18:32:28 -0500

    readded utils

---
+++ utils/trajectory.py
@@ -0,0 +1,24 @@
+from typing import Type, List
+
+from wpimath.geometry import Pose2d
+from wpimath.kinematics import DifferentialDriveKinematics
+from wpimath.trajectory import TrajectoryConfig, TrajectoryGenerator
+
+from utils.paths import Path, Route, GeneratedRoute
+import constants
+
+
+def generate_trajectory(path: Type[Path]) -> list[GeneratedRoute]:
+    return list(generate_route(path, r) for r in path.routes)
+
+
+def generate_route(path: Type[Path], route: Route) -> GeneratedRoute:
+    trajectory_config = TrajectoryConfig(path.max_velocity, path.max_acceleration)
+    trajectory_config.setReversed(False)
+    kinematics = DifferentialDriveKinematics(constants.track_width_meters)
+    trajectory_config.setKinematics(kinematics)
+    trajectory = TrajectoryGenerator.generateTrajectory(
+        route.start_pos, route.waypoints, route.end_pos,
+        trajectory_config
+    )
+    return GeneratedRoute(route, trajectory)
