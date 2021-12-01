e0f428d3842b08ae59fed7a1074b00e06184a5df
ethan <ethan.chapman0@outlook.com>
2021-12-01 13:35:36 -0500

    started swerve implement

--- subsystem/drivetrain.py
+++ subsystem/drivetrain.py
@@ -1,11 +1,16 @@
-from lib.motor import PIDMotor
-from lib.motors.ctre_motors import TalonConfig, TalonGroup, TalonFX
-from lib.subsystem_templates.drivetrain.differential_drivetrain import DifferentialDrivetrain
+from lib.subsystem_templates.drivetrain.swerve_drivetrain import SwerveDrivetrain, SwerveNode
 
 
-_MOTOR_CONFIG = TalonConfig(0.5, 0, 0, 1, 15000, 10000)
+class NEOSwerveNode(SwerveNode):
+    def init(self):
+        pass
+
+    def set_angle_raw(self, pos: float):
+        pass
+
+    def set_velocity_raw(self, vel_tw_per_second: float):
+        pass
 
 
-class Drivetrain(DifferentialDrivetrain):
-    m_left: PIDMotor = TalonGroup(TalonFX(0), TalonFX(1), TalonFX(2), config=_MOTOR_CONFIG)
-    m_right: PIDMotor = TalonGroup(TalonFX(3), TalonFX(4), TalonFX(5), config=_MOTOR_CONFIG)
+class Drivetrain(SwerveDrivetrain):
+    n_00 =
