cbffe14dd6cefd8bb939f016e7ed7597b02476fb
ethan <ethan.chapman0@outlook.com>
2022-02-20 11:30:13 -0500

    move shooter gear ratios

--- constants.py
+++ constants.py
@@ -1,6 +1,8 @@
 from robotpy_toolkit_7407.motors.ctre_motors import talon_sensor_unit, talon_sensor_vel_unit, talon_sensor_accel_unit
 from robotpy_toolkit_7407.utils.units import rad, rev, inch, deg, m, mile, hour, s, ft
 
+# --- DRIVETRAIN ---
+
 drivetrain_turn_gear_ratio = 3353.33 * talon_sensor_unit/rad
 drivetrain_move_gear_ratio = (511 * inch) / (544318 * talon_sensor_unit)
 
@@ -9,3 +11,13 @@ track_width = 29 * inch  # TODO Verify
 # TODO Maybe change these
 drivetrain_max_vel = 20 * mile/hour
 drivetrain_max_angular_vel = 2 * rev/s
+
+
+# --- SHOOTER ---
+
+# For m/s (ball exit velocity) to rad/s (motor velocity)
+shooter_top_gear_ratio = 1 * rad/m  # TODO Find this
+shooter_bottom_gear_ratio = 1 * rad/m  # TODO Find this
+
+# Shooter angle to motor angle
+shooter_angle_gear_ratio = 1 * rad/rad  # TODO Find this
--- subsystem/shooter.py
+++ subsystem/shooter.py
@@ -3,13 +3,7 @@ from robotpy_toolkit_7407.motors import TalonFX, TalonGroup, TalonConfig
 from robotpy_toolkit_7407.utils.units import rad, m, s
 from unum import Unum
 
-
-# For m/s (ball exit velocity) to rad/s (motor velocity)
-TOP_GEAR_RATIO = 1 * rad/m  # TODO Find this
-BOTTOM_GEAR_RATIO = 1 * rad/m  # TODO Find this
-
-# Shooter angle to motor angle
-ANGLE_GEAR_RATIO = 1 * rad/rad  # TODO Find this
+import constants
 
 
 class Shooter(Subsystem):
@@ -24,8 +18,8 @@ class Shooter(Subsystem):
         self.m_angle.init()
 
     def set_angle(self, theta: Unum):
-        self.m_angle.set_target_position(theta * ANGLE_GEAR_RATIO)
+        self.m_angle.set_target_position(theta * constants.shooter_angle_gear_ratio)
 
     def set_flywheels(self, top_vel: Unum, bottom_vel: Unum):
-        self.m_top.set_target_velocity(top_vel * TOP_GEAR_RATIO)
-        self.m_bottom.set_target_velocity(bottom_vel * BOTTOM_GEAR_RATIO)
+        self.m_top.set_target_velocity(top_vel * constants.shooter_top_gear_ratio)
+        self.m_bottom.set_target_velocity(bottom_vel * constants.shooter_bottom_gear_ratio)
