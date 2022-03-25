8511c1227b7893df059366f668a9a0d26475042e
Sidharth Rao <sidharthmrao@gmail.com>
2022-03-25 17:05:56 -0400

    Fixed reversed motor

--- command/intake.py
+++ command/intake.py
@@ -3,8 +3,8 @@ from commands2 import InstantCommand
 from robot_systems import Robot
 
 
-IntakeToggleLeft = lambda: InstantCommand(Robot.intake.toggle_left_intake, Robot.intake)
-IntakeToggleRight = lambda: InstantCommand(Robot.intake.toggle_right_intake, Robot.intake)
-IntakeDinglebobOn = lambda: InstantCommand(lambda: Robot.intake.dinglebobs_in, Robot.intake)
-IntakeDinglebobOff = lambda: InstantCommand(lambda: Robot.intake.dinglebobs_off, Robot.intake)
-IntakeToggleReverse = lambda: InstantCommand(Robot.intake.dinglebob_eject_left, Robot.intake)
+IntakeToggleLeft = lambda: InstantCommand(Robot.intake.toggle_left, Robot.intake)
+IntakeToggleRight = lambda: InstantCommand(Robot.intake.toggle_right, Robot.intake)
+IntakeDinglebobOn = lambda: InstantCommand(lambda: Robot.intake.m_top.set_raw_output(0.5), Robot.intake)
+IntakeDinglebobOff = lambda: InstantCommand(lambda: Robot.intake.m_top.set_raw_output(0), Robot.intake)
+IntakeToggleReverse = lambda: InstantCommand(Robot.intake.toggle_reverse, Robot.intake)
