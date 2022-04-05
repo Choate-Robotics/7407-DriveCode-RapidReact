9a2c522e87cb802095678824b8cc74f2db2ec27b
ethan <ethan.chapman0@outlook.com>
2022-04-04 22:44:01 -0400

    camera server work

--- sensors/intake_camera_network.py
+++ sensors/intake_camera_network.py
@@ -1,21 +1,24 @@
 import socket
 
 
+PORT = 37201
+
+
 class UDPServer:
     def __init__(self):
         self._socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
         self._socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-        self._socket.bind(('', 5804))
+        self._socket.bind(('', PORT))
 
     def send_packet(self, data: bytes):
-        self._socket.sendto(data, ("<broadcast>", 5804))
+        self._socket.sendto(data, ("<broadcast>", PORT))
 
 
 class UDPClient:
     def __init__(self):
         self._socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
         self._socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-        self._socket.bind(('', 5804))
+        self._socket.bind(('', PORT))
 
     def recv_packet(self) -> bytes:
         return self._socket.recvfrom(500)[0]
--- sensors/intake_camera_server_test.py
+++ sensors/intake_camera_server_test.py
@@ -5,5 +5,4 @@ from sensors.intake_camera_network import UDPServer
 server = UDPServer()
 
 while True:
-    time.sleep(1)
     server.send_packet(b"hello world")
