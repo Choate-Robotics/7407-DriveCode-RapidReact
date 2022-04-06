c5b41127c1c7ff8515487eaba2554dbfaecf02f8
mbardoe <mbardoe@gmail.com>
2022-04-06 14:48:44 -0400

    Aim didn't work back to degrees.

--- command/drivetrain.py
+++ command/drivetrain.py
@@ -60,15 +60,15 @@ class DriveSwerveAim(SubsystemCommand[SwerveDrivetrain]):
 
     def initialize(self) -> None:
         Robot.limelight.ref_on()
-        self.old_limelight=Robot.limelight.get_x_offset().asNumber(rad)
+        self.old_limelight=Robot.limelight.get_x_offset().asNumber(rad) #Bardoe for the damping
 
     def execute(self) -> None:
         dx, dy = self.subsystem.axis_dx.value, self.subsystem.axis_dy.value
-        #omega = self.controller.calculate(0, self.cam.get_x_offset()) * rad / s
-        d_omega=self.old_limelight-Robot.limelight.get_x_offset().asNumber(rad) # BARDOE
+        #omega = self.controller.calculate(0, self.cam.get_x_offset()) * rad / s #NOT BARDOE
+        d_omega=self.old_limelight-Robot.limelight.get_x_offset().asNumber(deg) # BARDOE
         #omega = -0.06 * Robot.limelight.get_x_offset()* rad / s #(The 3 is adjustable, p-gain) #.07
-        omega = (-0.4 * Robot.limelight.get_x_offset().asNumber(rad) + .05*d_omega)* rad / s  # BARDOE (tune constants)
-        self.old_limelight=Robot.limelight.get_x_offset().asNumber(rad) #BARDOE
+        omega = (-0.4 * Robot.limelight.get_x_offset().asNumber(deg) + .05*d_omega)* rad / s  # BARDOE (tune constants)
+        self.old_limelight=Robot.limelight.get_x_offset().asNumber(deg) #BARDOE
 
         dx = curve(dx)
         dy = curve(dy)
@@ -78,7 +78,7 @@ class DriveSwerveAim(SubsystemCommand[SwerveDrivetrain]):
         dy *= -self.subsystem.max_vel.asUnit(m / s)
 
         #if abs(Robot.drivetrain.chassis_speeds.omega) < .1 and Robot.limelight.get_x_offset() != 0:
-        if abs(Robot.drivetrain.chassis_speeds.omega) < .1:# BARDOE Why did we have it so the x_offset is not zero?
+        if abs(Robot.drivetrain.chassis_speeds.omega) < .1 and Robot.limelight.get_x_offset(): # BARDOE Why did we have it so the x_offset is not zero?
 
             self.c_count += 1 # Why do we do this?
             if self.c_count >= self.ready_counts:
