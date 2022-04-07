947ee134b6f9fbeca4b3c44601f1a12d8eec4954
Sidharth Rao <sidharthmrao@gmail.com>
2022-04-07 18:23:20 -0400

    IT WORKSSSSS

--- subsystem/intake.py
+++ subsystem/intake.py
@@ -105,6 +105,10 @@ class Intake(Subsystem):
             self.left_intake_motor.set_raw_output(self.intake_speed)
         if self.right_intake_down:
             self.right_intake_motor.set_raw_output(self.intake_speed)
+        if not self.left_intake_down:
+            self.left_intake_motor.set_raw_output(0)
+        if not self.right_intake_down:
+            self.right_intake_motor.set_raw_output(0)
 
     def dinglebobs_out(self):
         self.left_dinglebob.set_raw_output(self.dinglebob_speed)
